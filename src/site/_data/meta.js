7cb4b731c0d5a3709a092cdd3ebd11abe2ac6cc2
Ole Eskild Steensen <ole.steensen@knowit.no>
2022-03-14 23:51:58 +0100

    Fetch serverside

--- src/site/_data/meta.js
+++ src/site/_data/meta.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
+let themeStyle = "";
+if (process.env.THEME) {
+    fetch(process.env.THEME).then(response => response.text()).then(text => {
+        themeStyle = "<style>" + text + "</style>";
+    });
+}
 module.exports = {
     env: process.env.ELEVENTY_ENV,
     theme: process.env.THEME,
+    themeStyle: themeStyle,
     baseTheme: process.env.BASE_THEME || "dark"
 };
