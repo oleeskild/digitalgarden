9d2eed43d129001d5552c9f436213136bfdf53b0
Ole Eskild Steensen <ole@savvy.no>
2022-11-27 15:16:44 +0100

    Handle themes not adhering to obsidian v1

--- src/site/_data/meta.js
+++ src/site/_data/meta.js
@@ -4,8 +4,22 @@ const axios = require("axios");
 
 module.exports = async() => {
     let themeStyle = "";
-    if (process.env.THEME) {
-        const res = await axios.get(process.env.THEME)
+    let themeUrl = process.env.THEME;
+    if (themeUrl) {
+        //https://forum.obsidian.md/t/1-0-theme-migration-guide/42537
+        //Not all themes with no legacy mark have a theme.css file, so we need to check for it
+        try{
+            await axios.get(themeUrl);
+        }catch{
+            if(themeUrl.indexOf("theme.css") > -1){
+                themeUrl = themeUrl.replace("theme.css", "obsidian.css");
+            }
+            else if(themeUrl.indexOf("obsidian.css") > -1){
+                themeUrl = themeUrl.replace("obsidian.css", "theme.css");
+            }
+        }
+
+        const res = await axios.get(themeUrl);
         themeStyle = `<style>${res.data}</style>`;
     }
     const meta ={
