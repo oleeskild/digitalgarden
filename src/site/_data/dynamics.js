705e80e5f2f33cf84bb1031217bc46e097a7c0d9
Utsob Roy <roy@utsob.me>
2023-02-06 14:29:23 +0600

    dynamic import implementation

---
+++ src/site/_data/dynamics.js
@@ -0,0 +1,33 @@
+const fsFileTree = require("fs-file-tree");
+
+const BASE_PATH = "src/site/_includes/components/user"
+const NAMESPACES = ["index", "notes", "common"];
+const SLOTS = ["header", "afterContent", "footer"]
+
+const generateComponentPaths = async (namespace) => {
+    const data = {};
+    for (let index = 0; index < SLOTS.length; index++) {
+        const slot = SLOTS[index];
+        try {
+            console.log(`${BASE_PATH}/${namespace}/${slot}`);
+            const tree = await fsFileTree(`${BASE_PATH}/${namespace}/${slot}`);
+            let comps = Object.keys(tree).filter((p) => p.indexOf(".njk") != -1).map((p) => `components/user/${namespace}/${slot}/${p}`);
+            comps.sort()
+            data[slot] = comps;
+        } catch {
+            data[slot] = [];
+        }
+    }
+    return data;
+}
+
+
+module.exports = async () => {
+    const data = {};
+    for (let index = 0; index < NAMESPACES.length; index++) {
+        const ns = NAMESPACES[index];
+        data[ns] = await generateComponentPaths(ns);
+    }
+    console.log(data);
+    return data;
+}
