acfea37940ea82ba85266138fa1e030d0103b610
Utsob Roy <roy@utsob.me>
2023-02-17 21:18:49 +0600

    user computed similar to custom-style (#78)

--- src/site/_data/eleventyComputed.js
+++ src/site/_data/eleventyComputed.js
@@ -1,5 +1,7 @@
 const { getGraph } = require("../../helpers/linkUtils");
+const { userComputed } = require("../../helpers/userUtils");
 
 module.exports = {
-    graph: (data) => getGraph(data),
-}
+  graph: (data) => getGraph(data),
+  userComputed: (data) => userComputed(data),
+};
