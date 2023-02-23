b559425c7b725ce35d1bfd638f9e44df05fc3787
Utsob Roy <roy@utsob.me>
2023-02-23 16:34:43 +0600

    footnote fix via better way

--- .eleventy.js
+++ .eleventy.js
@@ -18,6 +18,9 @@ module.exports = function (eleventyConfig) {
     breaks: true,
     html: true,
   })
+    .use(require("markdown-it-anchor"), {
+      slugify: headerToId,
+    })
     .use(require("markdown-it-mark"))
     .use(require("markdown-it-footnote"))
     .use(function (md) {
@@ -139,22 +142,6 @@ module.exports = function (eleventyConfig) {
         }
 
         return defaultLinkRule(tokens, idx, options, env, self);
-      };
-      // Footnote heading fix (till the upstream releases the fix)
-      md.renderer.rules.render_footnote_anchor_name = (
-        tokens,
-        idx,
-        options,
-        env
-      ) => {
-        var n = Number(tokens[idx].meta.id + 1).toString();
-        var prefix = "";
-
-        if (env && typeof env.docId === "string") {
-          prefix = "-" + env.docId + "-";
-        }
-
-        return prefix + n;
       };
     });
 
