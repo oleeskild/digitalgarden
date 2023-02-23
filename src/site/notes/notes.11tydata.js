1394822d5040e9b383efe23eafc8e10dbc7ce401
Utsob Roy <roy@utsob.me>
2023-02-23 11:21:09 +0600

    single homepage rendering

--- src/site/notes/notes.11tydata.js
+++ src/site/notes/notes.11tydata.js
@@ -5,6 +5,18 @@ const allSettings = settings.ALL_NOTE_SETTINGS;
 
 module.exports = {
   eleventyComputed: {
+    layout: (data) => {
+      if (data.tags.indexOf("gardenEntry") != -1) {
+        return "layouts/index.njk";
+      }
+      return "layouts/note.njk";
+    },
+    permalink: (data) => {
+      if (data.tags.indexOf("gardenEntry") != -1) {
+        return "/";
+      }
+      return data.permalink || undefined;
+    },
     settings: (data) => {
       const noteSettings = {};
       allSettings.forEach((setting) => {
