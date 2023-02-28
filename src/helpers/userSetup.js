6e63aab6f5676fbf434f3d3c2006bf87671fd27e
Utsob Roy <roy@utsob.me>
2023-02-28 13:59:29 +0600

    comments on function

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -1,6 +1,10 @@
-// Put your computations here.
-
-function userMarkdownSetup(md) {}
-function userEleventySetup(eleventyConfig) {}
+function userMarkdownSetup(md) {
+  // The md parameter stands for the markdown-it instance used throughout the site generator.
+  // Feel free to add any plugin you want here instead of /.eleventy.js
+}
+function userEleventySetup(eleventyConfig) {
+  // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
+  // Feel free to add any plugin you want here instead of /.eleventy.js
+}
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
