12575e1141ca7e7e517c38506745e6463469aee3
Utsob Roy <roy@utsob.me>
2023-03-07 02:41:14 +0600

    timestamp support (#81)

--- src/site/_data/meta.js
+++ src/site/_data/meta.js
@@ -38,12 +38,18 @@ module.exports = async () => {
     bodyClasses.push("links-note-icon");
     noteIconsSettings.links = true;
   }
+  let timestampSettings = {
+    timestampFormat: process.env.TIMESTAMP_FORMAT || "MMM dd, yyyy h:mm a",
+    showCreated: process.env.SHOW_CREATED_TIMESTAMP == "true",
+    showUpdated: process.env.SHOW_UPDATED_TIMESTAMP == "true",
+  };
   const meta = {
     env: process.env.ELEVENTY_ENV,
     theme: process.env.THEME,
     themeStyle,
     bodyClasses: bodyClasses.join(" "),
     noteIconsSettings,
+    timestampSettings,
     baseTheme: process.env.BASE_THEME || "dark",
     siteName: process.env.SITE_NAME_HEADER || "Digital Garden",
     siteBaseUrl: baseUrl,
