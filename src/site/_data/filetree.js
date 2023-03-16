127e7adb5d250b44cc51f9f09c6d1f5b807f04c1
Ole Eskild Steensen <ole@savvy.no>
2023-03-16 21:00:03 +0100

    Check that tags exist before checking for entry

--- src/site/_data/filetree.js
+++ src/site/_data/filetree.js
@@ -53,7 +53,7 @@ function getPermalinkMeta(path, key) {
     if (frontMatter.data.permalink) {
       permalink = frontMatter.data.permalink;
     }
-    if (frontMatter.data.tags.indexOf("gardenEntry") != -1) {
+    if (frontMatter.data.tags && frontMatter.data.tags.indexOf("gardenEntry") != -1) {
       permalink = "/";
     }
     if (frontMatter.data.title) {
