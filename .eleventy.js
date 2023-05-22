9d45cde0e630c2f97a0d8f7495d7e4664bbfa8f3
Ole Eskild Steensen <oleeskild@users.noreply.github.com>
2023-05-22 19:03:06 +0200

    Use correct parameter for setting favicon outpu

--- .eleventy.js
+++ .eleventy.js
@@ -433,7 +433,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/site/img");
   eleventyConfig.addPassthroughCopy("src/site/scripts");
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
-  eleventyConfig.addPlugin(faviconsPlugin, { destination: "dist" });
+  eleventyConfig.addPlugin(faviconsPlugin, { outputDir: "dist" });
   eleventyConfig.addPlugin(tocPlugin, {
     ul: true,
     tags: ["h1", "h2", "h3", "h4", "h5", "h6"],
