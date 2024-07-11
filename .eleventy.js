422654534915a0d558688e35c16c997ac76a3ead
Ole Eskild Steensen <oleeskild@users.noreply.github.com>
2024-07-11 14:48:00 +0200

    Bump 11ty/eleventy-img and change html minifier library

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ const matter = require("gray-matter");
 const faviconsPlugin = require("eleventy-plugin-gen-favicons");
 const tocPlugin = require("eleventy-plugin-nesting-toc");
 const { parse } = require("node-html-parser");
-const htmlMinifier = require("html-minifier");
+const htmlMinifier = require("html-minifier-terser");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 const { headerToId, namedHeadingsFilter } = require("./src/helpers/utils");
