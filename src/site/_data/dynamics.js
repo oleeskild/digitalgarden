a0b3c2e766ef3a11089d34bfc1bee5a33d08de2c
WheresAlice <wheresalice@noreply.users.github.com>
2025-09-23 10:42:22 +0100

    upgrade to eleventy v3
    convert to ESM by adding `"type":"module"` and running `find  -name '*.js' -type 'f' -not -path './node_modules/*' | xargs -I {}  npx -p typescript@4.9.5 -p cjstoesm cjstoesm {}`
    
    fix broken imports caused by the conversion script
    
    follow the [upgrader helper](https://www.11ty.dev/docs/plugins/upgrade-help/) docs
    
    fix the `getGraph(data)` function to be async and access contect correctly
    
    fix broken syntax in `assignNested` function
    
    upgrade `@11ty/eleventy-plugin-rss` while we're at it (not actually
    required, but may as well)

--- src/site/_data/dynamics.js
+++ src/site/_data/dynamics.js
@@ -1,5 +1,4 @@
-const fsFileTree = require("fs-file-tree");
-
+import fsFileTree from "fs-file-tree";
 const BASE_PATH = "src/site/_includes/components/user";
 const STYLE_PATH = "src/site/styles/user";
 const NAMESPACES = ["index", "notes", "common"];
@@ -9,52 +8,43 @@ const FILE_TREE_SLOTS = ["beforeTitle", "afterTitle"];
 const SIDEBAR_NAMESPACE = "sidebar";
 const SIDEBAR_SLOTS = ["top", "bottom"];
 const STYLES_NAMESPACE = "styles";
-
 const generateComponentPaths = async (namespace, slots) => {
-  const data = {};
-  for (let index = 0; index < slots.length; index++) {
-    const slot = slots[index];
-    try {
-      const tree = await fsFileTree(`${BASE_PATH}/${namespace}/${slot}`);
-      let comps = Object.keys(tree)
-        .filter((p) => p.indexOf(".njk") != -1)
-        .map((p) => `components/user/${namespace}/${slot}/${p}`);
-      comps.sort();
-      data[slot] = comps;
-    } catch {
-      data[slot] = [];
+    const data = {};
+    for (let index = 0; index < slots.length; index++) {
+        const slot = slots[index];
+        try {
+            const tree = await fsFileTree(`${BASE_PATH}/${namespace}/${slot}`);
+            let comps = Object.keys(tree)
+                .filter((p) => p.indexOf(".njk") != -1)
+                .map((p) => `components/user/${namespace}/${slot}/${p}`);
+            comps.sort();
+            data[slot] = comps;
+        }
+        catch {
+            data[slot] = [];
+        }
     }
-  }
-  return data;
+    return data;
 };
-
 const generateStylesPaths = async () => {
-  try {
-    const tree = await fsFileTree(`${STYLE_PATH}`);
-    let comps = Object.keys(tree).map((p) =>
-      `/styles/user/${p}`.replace(".scss", ".css")
-    );
-    comps.sort();
-    return comps;
-  } catch {
-    return [];
-  }
+    try {
+        const tree = await fsFileTree(`${STYLE_PATH}`);
+        let comps = Object.keys(tree).map((p) => `/styles/user/${p}`.replace(".scss", ".css"));
+        comps.sort();
+        return comps;
+    }
+    catch {
+        return [];
+    }
 };
-
-module.exports = async () => {
-  const data = {};
-  for (let index = 0; index < NAMESPACES.length; index++) {
-    const ns = NAMESPACES[index];
-    data[ns] = await generateComponentPaths(ns, SLOTS);
-  }
-  data[FILE_TREE_NAMESPACE] = await generateComponentPaths(
-    FILE_TREE_NAMESPACE,
-    FILE_TREE_SLOTS
-  );
-  data[SIDEBAR_NAMESPACE] = await generateComponentPaths(
-    SIDEBAR_NAMESPACE,
-    SIDEBAR_SLOTS
-  );
-  data[STYLES_NAMESPACE] = await generateStylesPaths();
-  return data;
+export default async () => {
+    const data = {};
+    for (let index = 0; index < NAMESPACES.length; index++) {
+        const ns = NAMESPACES[index];
+        data[ns] = await generateComponentPaths(ns, SLOTS);
+    }
+    data[FILE_TREE_NAMESPACE] = await generateComponentPaths(FILE_TREE_NAMESPACE, FILE_TREE_SLOTS);
+    data[SIDEBAR_NAMESPACE] = await generateComponentPaths(SIDEBAR_NAMESPACE, SIDEBAR_SLOTS);
+    data[STYLES_NAMESPACE] = await generateStylesPaths();
+    return data;
 };
