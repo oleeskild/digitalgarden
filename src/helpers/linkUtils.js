25438ae73dc5509823b8569579b37a2f1e674594
Ole Eskild Steensen <oleeskild@users.noreply.github.com>
2025-11-13 13:24:50 +0100

    Upgrade to 11ty v3 (#331)
    * Upgraded to 11ty v3
    
    * update packages with security fixes available

--- src/helpers/linkUtils.js
+++ src/helpers/linkUtils.js
@@ -1,10 +1,6 @@
 const wikiLinkRegex = /\[\[(.*?\|.*?)\]\]/g;
 const internalLinkRegex = /href="\/(.*?)"/g;
 
-function caselessCompare(a, b) {
-  return a.toLowerCase() === b.toLowerCase();
-}
-
 function extractLinks(content) {
   return [
     ...(content.match(wikiLinkRegex) || []).map(
@@ -30,18 +26,27 @@ function extractLinks(content) {
   ];
 }
 
-function getGraph(data) {
+async function getGraph(data) {
   let nodes = {};
   let links = [];
   let stemURLs = {};
   let homeAlias = "/";
-  (data.collections.note || []).forEach((v, idx) => {
+
+  // Process notes sequentially to handle async reads
+  const notes = data.collections.note || [];
+  for (let idx = 0; idx < notes.length; idx++) {
+    const v = notes[idx];
     let fpath = v.filePathStem.replace("/notes/", "");
     let parts = fpath.split("/");
     let group = "none";
     if (parts.length >= 3) {
       group = parts[parts.length - 2];
     }
+
+    // Use async read() method instead of accessing frontMatter directly
+    const templateContent = await v.template.read();
+    const content = templateContent?.content || "";
+
     nodes[v.url] = {
       id: idx,
       title: v.data.title || v.fileSlug,
@@ -51,7 +56,7 @@ function getGraph(data) {
         v.data["dg-home"] ||
         (v.data.tags && v.data.tags.indexOf("gardenEntry") > -1) ||
         false,
-      outBound: extractLinks(v.template.frontMatter.content),
+      outBound: extractLinks(content),
       neighbors: new Set(),
       backLinks: new Set(),
       noteIcon: v.data.noteIcon || process.env.NOTE_ICON_DEFAULT,
@@ -64,7 +69,7 @@ function getGraph(data) {
     ) {
       homeAlias = v.url;
     }
-  });
+  }
   Object.values(nodes).forEach((node) => {
     let outBound = new Set();
     node.outBound.forEach((olink) => {
--- src/site/_data/eleventyComputed.js
+++ src/site/_data/eleventyComputed.js
@@ -3,7 +3,7 @@ const { getFileTree } = require("../../helpers/filetreeUtils");
 const { userComputed } = require("../../helpers/userUtils");
 
 module.exports = {
-  graph: (data) => getGraph(data),
+  graph: async (data) => await getGraph(data),
   filetree: (data) => getFileTree(data),
   userComputed: (data) => userComputed(data)
 };
