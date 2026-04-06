f656c66654f6d7365ea6498fd9ccafe051a8679e
Ole Eskild Steensen <6201338+oleeskild@users.noreply.github.com>
2026-04-06 22:27:38 +0200

    Add noteProps alias for dg-note-properties object
    nunjuck does not support "-" character for properties.
    So we instead alias it to noteProps. That way you can use
    {{noteProps.fieldName}} in your nunjuck templates.

--- src/site/_data/eleventyComputed.js
+++ src/site/_data/eleventyComputed.js
@@ -5,5 +5,6 @@ const { userComputed } = require("../../helpers/userUtils");
 module.exports = {
   graph: async (data) => await getGraph(data),
   filetree: (data) => getFileTree(data),
-  userComputed: (data) => userComputed(data)
+  userComputed: (data) => userComputed(data),
+  noteProps: (data) => data["dg-note-properties"]
 };
